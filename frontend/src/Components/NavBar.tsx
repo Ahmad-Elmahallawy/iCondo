5b081e9c15a39ba2e1d532faafafbd4c5f7c445b
Khanh Huy Nguyen <huy2272@gmail.com>
2024-01-29 23:49:22 -0500

    Added navbar and website's landing page (aka Hero), added styling to navbar to make it responsive.

---
+++ frontend/src/Components/NavBar.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import { Link } from "react-router-dom";
+import './NavBar.css'
+
+const Navbar = () => {
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => setClick(!click)
+
+    return (
+        <div className='navbar'>
+            <div className='logo'>
+                <img width="130" height="130" src={'Assets/logo-placeholder.svg'} alt='logo' />
+            </div>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                <li className='nav-item'><Link to="/">Home</Link></li>
+                <li className='nav-item'><Link to="/Login">Log In</Link></li>
+                <li className='nav-item'><Link to="/Register">Register</Link></li>
+                <li className='nav-item'><Link to="/Signup">Sign Up</Link></li>
+            </ul>
+            <div className='hamburger' onClick={handleClick}>
+                {click ? (<FaTimes size={30} style={{ color: '#f8f8f8' }} />) : (<FaBars size={30} style={{ color: '#f8f8f8' }} />)}
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
---
+++ frontend/src/Pages/Hero.tsx
@@ -0,0 +1,28 @@
+import './Hero.css'
+
+const Hero = () => {
+    return (
+        <div className='hero'>
+            <div className='container'>
+                <div className='content'>
+                    <div className='col-1'>
+                        <h1>Welcome to your new home</h1>
+                        <p>Lorem ipsum dolor sit amet, consec tetur adipisicing elit.
+                            Architecto iure fuga deleniti sit! Cum doloribus, nesciunt
+                            laboriosam eos praesentium veritatis!</p>
+                    </div>
+                    <div className='col-2'>
+                        <div className='img-layout'>
+                            <div className='img-container'>
+                                <img src="Assets/registration-room.png" alt="" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Hero
