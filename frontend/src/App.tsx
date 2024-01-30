2dbd58c58668d1e2ca5ff3dd73b89761a9505b0a
Ahmad-Elmahallawy <ahmad.mahallawy@gmail.com>
2024-01-30 15:36:11 -0500

    fixed merge conflicts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './Style/root.css'
 import SignUp from './Components/Authentication/SignUp';
-import RegistrationLandingPage from './Components/Authentication/RegistrationLandingPage';
-import LogInLandingPage from './Components/Authentication/LogInLandingPage';
+import RegistrationLandingPage from './Pages/RegistrationLandingPage';
+import LogInLandingPage from './Pages/LogInLandingPage';
+import NavBar from './Components/NavBar';
+import Hero from './Pages/Hero';
+
 
 function App() {
   return (
-    <div className="App">
-     <LogInLandingPage />
-    </div>
+    <BrowserRouter>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Hero />} />
+        <Route path="Login" element={<LogInLandingPage />} />
+        <Route path="Register" element={<RegistrationLandingPage />} />
+        <Route path="Signup" element={<SignUp />} />
+        <Route path="Portfolio" element={<></>} />
+        <Route path="Finance" element={<></>} />
+        <Route path="*" element={<></>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
