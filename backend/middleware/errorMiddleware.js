ea92d5933c3e2f8d02a102ffbb9f365aee367066
HoangMinhKhoiPham <97756628+HoangMinhKhoiPham@users.noreply.github.com>
2024-02-07 01:16:53 -0500

    errorMiddleWare test

--- backend/middleware/errorMiddleware.js
+++ backend/middleware/errorMiddleware.js
@@ -1,14 +1,14 @@
 const errorHandler = (err, req, res, next) => {
-    const statusCode = res.statusCode ? res.statusCode : 500
-
-    res.status(statusCode)
-
-    res.json({
+    let statusCode = res.statusCode;
+    if (!statusCode || parseInt(statusCode) < 100 || parseInt(statusCode) >= 600) {
+        statusCode = 500;
+    }
+    res.status(statusCode).json({
         message: err.message,
         stack: process.env.NODE_ENV === 'production' ? null : err.stack,
-    })
-}
+    });
+};
 
 module.exports = {
     errorHandler,
-}
+};
