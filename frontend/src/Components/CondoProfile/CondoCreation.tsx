580038567c8a58733dcc9759f0d7876bd73461ca
Ahmad-Elmahallawy <ahmad.mahallawy@gmail.com>
2024-02-18 20:09:14 -0500

    instantiated the files needed for the page

---
+++ frontend/src/Components/CondoProfile/CondoCreation.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CondoCreation = () => {
+  return (
+    <div>CondoCreation</div>
+  )
+}
+
+export default CondoCreation
---
+++ frontend/src/Pages/CondoCreationLandingPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CondoCreationLandingPage = () => {
+  return (
+    <div>CondoCreationLandingPage</div>
+  )
+}
+
+export default CondoCreationLandingPage
