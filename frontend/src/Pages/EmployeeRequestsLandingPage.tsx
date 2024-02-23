5dfb0c863561bca04001362d36db29ff9142bf78
Ahmad-Elmahallawy <ahmad.mahallawy@gmail.com>
2024-02-23 01:44:51 -0500

    added comments for EmployeeRequestsLandingPage

--- frontend/src/Pages/EmployeeRequestsLandingPage.tsx
+++ frontend/src/Pages/EmployeeRequestsLandingPage.tsx
@@ -3,6 +3,13 @@ import React, { useState } from "react";
 import KeyGeneration from "../Components/Requests/KeyGeneration";
 import "../Style/LandingPageStyle/EmployeeRequestsLandingPageStyle.css";
 
+
+// EmployeeRequestsLandingPage Component:
+// This component represents the landing page for employee requests management.
+// It renders the KeyGeneration component from "../Components/Requests/KeyGeneration".
+// The component manages state for the selected user name using the useState hook.
+// It contains a function handleUserItemClick that updates the selected user name when a user item is clicked.
+// The selected user name is passed to the KeyGeneration component as a prop.
 const EmployeeRequestsLandingPage = () => {
   const [selectedUserName, setSelectedUserName] = useState<string>("");
 
