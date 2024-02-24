6b7e528ac9eb17786f32892e994f86bb259191f6
Ahmad-Elmahallawy <ahmad.mahallawy@gmail.com>
2024-02-24 01:39:11 -0500

    added key generation functionality

--- frontend/src/Components/Requests/KeyGeneration.tsx
+++ frontend/src/Components/Requests/KeyGeneration.tsx
@@ -1,21 +1,32 @@
 // KeyGeneration.tsx
-import React from "react";
+import React, { useState } from "react";
 import "../../Style/RequestsStyle/KeyGenerationStyle.css";
-
-interface KeyGenerationProps {
-  userName?: string; // Marking userName as optional
-}
-
 
 // KeyGeneration Component:
 // This component renders a form to generate registration keys for users.
-// It accepts one prop:
-// 1. userName: An optional string representing the name of the user. Defaults to "[Name Here]" if not provided.
 // The component displays the user's name, two buttons to specify whether the user is a condo owner or a rental user,
 // and a button to send the registration key to the user.
-const KeyGeneration: React.FC<KeyGenerationProps> = ({
-  userName = "[Name Here]",
-}) => {
+const KeyGeneration = () => {
+  const [registrationKey, setRegistrationKey] = useState(
+    "No Key To Show Right Now"
+  );
+
+  // function to randomly generate a key of size 6
+  const handleGenerateKey = () => {
+    let result = "",
+      length = 6;
+
+    const characters =
+      "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    const charactersLength = characters.length;
+    let counter = 0;
+    while (counter < length) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+      counter += 1;
+    }
+    setRegistrationKey(result);
+    // TODO: add API to link it to condo
+  };
   return (
     <div className="key-generation-container">
       <div className="key-generation-header">
@@ -23,15 +34,15 @@ const KeyGeneration: React.FC<KeyGenerationProps> = ({
       </div>
       <div className="key-generation-content">
         <div className="key-generation-user-result">
-          <p>User:</p>
-          <p>{userName}</p>
+          <p>Generated Key:</p>
+          <p>{registrationKey}</p>
         </div>
         <div className="key-generation-two-buttons">
           <button>Condo Owner</button>
           <button>Rental User</button>
         </div>
         <div className="key-generation-submission">
-          <button>Send Registration Key to User</button>
+          <button onClick={handleGenerateKey}>Generate Registration Key</button>
         </div>
       </div>
     </div>
