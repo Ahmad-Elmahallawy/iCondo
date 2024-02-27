44c5346cd1e22cb103c0d8a5673cdf532746cea8
HoangMinhKhoiPham <97756628+HoangMinhKhoiPham@users.noreply.github.com>
2024-02-27 00:52:09 -0500

    update signUp Form

--- condo-management-app/screens/auth/SignUpAsAdmin/index.js
+++ condo-management-app/screens/auth/SignUpAsAdmin/index.js
@@ -8,6 +8,7 @@ import Checkbox from "../../../Component/Checkbox";
 import Separator from "../../../Component/Separator";
 const SignUpAsAdmin = ({navigation}) => {
     const [checked, setChecked] = useState(false);
+    const [values, setValues] = useState({})
     const onSignIn = () => {
         navigation.navigate('SignIn')
     }
@@ -17,12 +18,13 @@ const SignUpAsAdmin = ({navigation}) => {
     return (
         <ScrollView style={styles.container}>
             <AuthHeader onBackPress={onBack} title="Sign Up" />
-
-            <Input label="Name" placeholder="John Doe" />
-            <Input label="E-mail" placeholder="example@gmail.com" />
-            <Input isPassword label="Password" placeholder="*******" />
-
-            <Separator/>
+            <Input value={values.companyName} onChangeText={(v) => onChange('companyName', v)} label="Company Name" placeholder="ABC Inc" />
+            <Input value={values.firstName} onChangeText={(v) => onChange('firstName', v)} label="First Name" placeholder="John" />
+            <Input value={values.lastName} onChangeText={(v) => onChange('lastName', v)} label="Last Name" placeholder="Doe" />
+            <Input value={values.userName} onChangeText={(v) => onChange('userName', v)} label="Username" placeholder="JohnDoe123" />
+            <Input value={values.email} onChangeText={(v)=> onChange('email', v)} label="E-mail" placeholder="example@gmail.com" />
+            <Input value={values.phoneNumber} onChangeText={(v)=> onChange('phoneNumber', v)} label="Phone number" placeholder="111-111-111" />
+            <Input value={values.password} onChangeText={(v) => onChange('password', v)} isPassword label="Password" placeholder="*******" />
             <Button style={styles.button} title="Sign Up"  />
             <Separator/>
             <Text style={styles.footerText}>
