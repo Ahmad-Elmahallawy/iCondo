a0907a9208c58ef3c8e332991a0cd4648116f3fc
Khanh Huy Nguyen <huy227@gmail.com>
2024-02-28 19:04:30 -0500

    Integrate register api key

--- frontend/src/Components/UserProfile/UserKeyRegister.tsx
+++ frontend/src/Components/UserProfile/UserKeyRegister.tsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import TextField from "@mui/material/TextField";
 import "../../Style/UserProfileStyle/UserInformationStyle.css";
 import { Box } from "@mui/material";
+import api from "../../api";
 
 // UserKeyRegister Component:
 // This component renders a form for user to submit registration key.
 const UserKeyRegister: React.FC = () => {
-  const submitHandler = (e: React.FormEvent<HTMLInputElement>) => {
+  const [key, setKey] = useState<string>("");
+  const [errorText, setErrorText] = React.useState<string>("");
+  const submitHandler = async (e: React.FormEvent<HTMLInputElement>) => {
     e.preventDefault();
-    console.log("submit");
+    try {
+      // Call api to register key
+      await api.registerationKeys.userRegisterKey(key);
+    } catch (error) {
+      setErrorText("Error registering key");
+    }
   };
   return (
     <Box
@@ -21,7 +29,14 @@ const UserKeyRegister: React.FC = () => {
       onSubmit={submitHandler}
     >
       <h2>Submit Registration Key</h2>
-      <TextField sx={{ padding: "10px" }} fullWidth id="registrationKey" />
+      <TextField
+        sx={{ padding: "10px" }}
+        fullWidth
+        id="registrationKey"
+        error={!!errorText}
+        helperText={errorText}
+        onInput={(e) => setKey((e.target as HTMLInputElement).value)}
+      />
       <button className="user-information-button" type="submit">
         Submit
       </button>
--- frontend/src/urls.ts
+++ frontend/src/urls.ts
@@ -7,6 +7,9 @@ const urls = {
     resetUserPassword: `${apiPrefixUserService}/users`,
     updateUserProfilePic: `${apiPrefix}/files`,
   },
+  registrationKeys: {
+    userRegister: `${apiPrefix}/registrationKeys`,
+  },
 };
 
 export default urls;
