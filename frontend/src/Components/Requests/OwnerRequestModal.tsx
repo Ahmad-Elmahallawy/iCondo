2b61305b928e478c940514cb2cffba05c462f1f3
Ahmad-Elmahallawy <ahmad.mahallawy@gmail.com>
2024-03-16 15:49:15 -0400

    added select choices for owner requests

--- frontend/src/Components/Requests/OwnerRequestModal.tsx
+++ frontend/src/Components/Requests/OwnerRequestModal.tsx
@@ -1,29 +1,55 @@
 // OwnerRequestModal.tsx
 
 import React from "react";
-import { Dialog, DialogActions, DialogContent, DialogTitle, Button } from "@mui/material";
-
+import {
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  Button,
+} from "@mui/material";
+import "../../Style/RequestsStyle/OwnerRequestModalStyle.css";
+import { StyledEngineProvider } from "@mui/material/styles";
+import SelectSubjectRequest from "./SelectSubjectRequest";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
 interface OwnerRequestModalProps {
   open: boolean;
   onClose: () => void;
 }
 
-const OwnerRequestModal: React.FC<OwnerRequestModalProps> = ({ open, onClose }) => {
+const OwnerRequestModal: React.FC<OwnerRequestModalProps> = ({
+  open,
+  onClose,
+}) => {
   return (
-    <Dialog open={open} onClose={onClose}>
-      <DialogTitle>Logout Confirmation</DialogTitle>
-      <DialogContent>
-        <p>Are you sure you want to log out?</p>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={onClose} color="primary">
-          Cancel
-        </Button>
-        <Button onClick={onClose} color="primary">
-          Log Out
-        </Button>
-      </DialogActions>
-    </Dialog>
+    <StyledEngineProvider injectFirst>
+      <Dialog
+        className="owner-request-modal-container"
+        open={open}
+        onClose={onClose}
+      >
+        <DialogTitle className="owner-request-modal-title">
+          New Request
+        </DialogTitle>
+        <DialogContent>
+          <p>Subject:</p>
+          <SelectSubjectRequest />
+        </DialogContent>
+        <DialogActions style={{ justifyContent: "space-between" }}>
+          <Button onClick={onClose} className="owner-request-modal-btn">
+            Submit
+          </Button>
+          <IconButton
+            onClick={onClose}
+            className="owner-request-modal-btn"
+            aria-label="delete"
+          >
+            <DeleteIcon />
+          </IconButton>
+        </DialogActions>
+      </Dialog>
+    </StyledEngineProvider>
   );
 };
 
