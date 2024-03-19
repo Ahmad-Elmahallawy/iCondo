e96279375acf5805b73bc851167689f4c7ffccb9
wolfie7679 <90850098+wolfie7679@users.noreply.github.com>
2024-03-19 17:55:15 -0400

    Added functioning notification consumer for creating and updating db stuff

--- server/src/notification/notification-consumer/notification-consumer.controller.ts
+++ server/src/notification/notification-consumer/notification-consumer.controller.ts
@@ -1,14 +1,61 @@
-import { Controller } from '@nestjs/common';
+import {Controller, Logger} from '@nestjs/common';
 import {EventPattern, Payload} from "@nestjs/microservices";
 import {KafkaMessage} from "../../kafka/KafkaMessage";
+import {NotificationService} from "../notification.service";
 
 @Controller('notification-consumer')
 export class NotificationConsumerController {
+    private readonly logger = new Logger(NotificationConsumerController.name);
+
+    constructor(private notificationService: NotificationService) {
+    }
 
     @EventPattern("request.status")
 
-    async onRequestStatus(@Payload() message: KafkaMessage){
-        console.log('MESSAGE INSIDE ANOTHER MICROSERVICE!!!!!')
-        console.log(message)
+    async onRequestStatus(@Payload() message: any) {
+        { // Use Message type
+            try {
+                // Pass the extracted data to createNotification
+                if(message.status==="CREATED")
+                await this.notificationService.createNotification({
+                    data: {
+                        createdAt: message.createdAt,
+                        id: message.id,
+                        message: JSON.stringify(message),
+                        title: message.status,
+                        updatedAt: message.updatedAt,
+                    },
+                    select: {
+                        createdAt: true,
+                        id: true,
+                        message: true,
+                        title: true,
+                        updatedAt: true,
+                    },
+                });
+
+                else await this.notificationService.updateNotification({
+                    where:{id:message.id},
+                    data: {
+                        createdAt: message.createdAt,
+                        id: message.id,
+                        message: JSON.stringify(message),
+                        title: message.status,
+                        updatedAt: message.updatedAt,
+                    },
+                    select: {
+                        createdAt: true,
+                        id: true,
+                        message: true,
+                        title: true,
+                        updatedAt: true,
+                    },
+                })
+
+                // this.logger.log(`Received message from topic ${topic}: ${message?.value?.toString() ?? ''}`);
+            } catch (error) {
+                this.logger.error(`Error creating notification: ${error}`);
+            }
+        }
     }
 }
--- server/src/request/base/request.service.base.ts
+++ server/src/request/base/request.service.base.ts
@@ -15,71 +15,76 @@ import {KafkaMessage} from "../../kafka/KafkaMessage";
 
 
 export class RequestServiceBase {
-  constructor(protected readonly prisma: PrismaService, protected readonly kafkaProducer: KafkaProducerService) {}
+    constructor(protected readonly prisma: PrismaService, protected readonly kafkaProducer: KafkaProducerService) {
+    }
 
-  async count<T extends Prisma.RequestCountArgs>(
-    args: Prisma.SelectSubset<T, Prisma.RequestCountArgs>
-  ): Promise<number> {
-    return this.prisma.request.count(args);
-  }
+    async count<T extends Prisma.RequestCountArgs>(
+        args: Prisma.SelectSubset<T, Prisma.RequestCountArgs>
+    ): Promise<number> {
+        return this.prisma.request.count(args);
+    }
 
-  async requests<T extends Prisma.RequestFindManyArgs>(
-    args: Prisma.SelectSubset<T, Prisma.RequestFindManyArgs>
-  ): Promise<Request[]> {
-    return this.prisma.request.findMany(args);
-  }
-  async getCondoUnit(parentId: string): Promise<CondoUnit | null> {
-    return this.prisma.request
-        .findUnique({
-          where: { id: parentId },
-        })
-        .condoUnit();
-  }
+    async requests<T extends Prisma.RequestFindManyArgs>(
+        args: Prisma.SelectSubset<T, Prisma.RequestFindManyArgs>
+    ): Promise<Request[]> {
+        return this.prisma.request.findMany(args);
+    }
 
-  async getEmployee(parentId: string): Promise<CompanyEmployee | null> {
-    return this.prisma.request
-        .findUnique({
-          where: { id: parentId },
-        })
-        .employee();
-  }
+    async getCondoUnit(parentId: string): Promise<CondoUnit | null> {
+        return this.prisma.request
+            .findUnique({
+                where: {id: parentId},
+            })
+            .condoUnit();
+    }
 
-  async getProperty(parentId: string): Promise<Property | null> {
-    return this.prisma.request
-        .findUnique({
-          where: { id: parentId },
-        })
-        .property();
-  }
-  async request<T extends Prisma.RequestFindUniqueArgs>(
-    args: Prisma.SelectSubset<T, Prisma.RequestFindUniqueArgs>
-  ): Promise<Request | null> {
-    return this.prisma.request.findUnique(args);
-  }
-  async createRequest<T extends Prisma.RequestCreateArgs>(
-    args: Prisma.SelectSubset<T, Prisma.RequestCreateArgs>
-  ): Promise<Request> {
-    const request:any = await this.prisma.request.create<T>(args);
+    async getEmployee(parentId: string): Promise<CompanyEmployee | null> {
+        return this.prisma.request
+            .findUnique({
+                where: {id: parentId},
+            })
+            .employee();
+    }
 
+    async getProperty(parentId: string): Promise<Property | null> {
+        return this.prisma.request
+            .findUnique({
+                where: {id: parentId},
+            })
+            .property();
+    }
+
+    async request<T extends Prisma.RequestFindUniqueArgs>(
+        args: Prisma.SelectSubset<T, Prisma.RequestFindUniqueArgs>
+    ): Promise<Request | null> {
+        return this.prisma.request.findUnique(args);
+    }
+
+    async createRequest<T extends Prisma.RequestCreateArgs>(
+        args: Prisma.SelectSubset<T, Prisma.RequestCreateArgs>
+    ): Promise<Request> {
+        const request: any = await this.prisma.request.create<T>(args);
+        request.status = "CREATED"
 
 
         const msg: KafkaMessage = {
             key: null,
-            value: {
-                "userID":request.user.id,
-                "id": request.id,
-                "status": 'CREATED'
-            }
+            value: request
         }
         await this.kafkaProducer.emitMessage(MyMessageBrokerTopics.RequestStatus, msg)
-        console.log('Message pushed to Topic')
         return request;
     }
 
     async updateRequest<T extends Prisma.RequestUpdateArgs>(
         args: Prisma.SelectSubset<T, Prisma.RequestUpdateArgs>
     ): Promise<Request> {
-        return this.prisma.request.update<T>(args);
+        const request = await this.prisma.request.update<T>(args);
+        const msg: KafkaMessage = {
+            key: null,
+            value: request
+        }
+        await this.kafkaProducer.emitMessage(MyMessageBrokerTopics.RequestStatus, msg)
+        return request
     }
 
     async deleteRequest<T extends Prisma.RequestDeleteArgs>(
@@ -88,19 +93,19 @@ export class RequestServiceBase {
         return this.prisma.request.delete(args);
     }
 
-  async getCompany(parentId: string): Promise<Company | null> {
-    return this.prisma.request
-      .findUnique({
-        where: { id: parentId },
-      })
-      .company();
-  }
+    async getCompany(parentId: string): Promise<Company | null> {
+        return this.prisma.request
+            .findUnique({
+                where: {id: parentId},
+            })
+            .company();
+    }
 
-  async getUser(parentId: string): Promise<User | null> {
-    return this.prisma.request
-      .findUnique({
-        where: { id: parentId },
-      })
-      .user();
-  }
+    async getUser(parentId: string): Promise<User | null> {
+        return this.prisma.request
+            .findUnique({
+                where: {id: parentId},
+            })
+            .user();
+    }
 }
