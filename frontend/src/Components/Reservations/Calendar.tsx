afd1ae39d86e30f457643d7033169aa3fd3f79f5
Ahmad-Elmahallawy <ahmad.mahallawy@gmail.com>
2024-03-29 16:24:16 -0400

    added calendar component and its css

--- frontend/src/Components/Reservations/Calendar.tsx
+++ frontend/src/Components/Reservations/Calendar.tsx
@@ -1,19 +1,45 @@
-import * as React from "react";
-import dayjs from "dayjs";
-import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
+import "../../Style/ReservationStyle/CalendarStyle.css";
 
-export default function DateCalendarReferenceDate() {
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin
+import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
+import timeGridPlugin from "@fullcalendar/timegrid";
+
+// Documentation for the following calendar: https://fullcalendar.io/docs/react
+export default function Calendar() {
+  const handleDateClick = (arg: any) => {
+    alert(arg.dateStr);
+  };
+  function renderEventContent(eventInfo: any) {
+    return (
+      <>
+        <b>{eventInfo.timeText}</b>
+        <i>{eventInfo.event.title}</i>
+      </>
+    );
+  }
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <DemoContainer components={["DateCalendar"]}>
-        <DateCalendar
-          referenceDate={dayjs("2023-04-17")}
-          views={["year", "month", "day"]}
-        />
-      </DemoContainer>
-    </LocalizationProvider>
+    <div className="calendar-container">
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
+        headerToolbar={{
+          left: "prev,next today",
+          center: "title",
+          right: "dayGridMonth,timeGridWeek,timeGridDay",
+        }}
+        initialView="dayGridMonth"
+        events={[
+          { title: "event 1", date: "2024-04-01" },
+          { title: "event 2", date: "2024-04-02" },
+        ]}
+        dateClick={handleDateClick} // handle day clicks
+        eventContent={renderEventContent}
+        themeSystem="standard"
+        editable={true}
+        selectable={true}
+        selectMirror={true}
+        dayMaxEvents={true}
+      />
+    </div>
   );
 }
