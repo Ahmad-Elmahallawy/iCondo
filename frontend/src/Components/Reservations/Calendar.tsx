06865aadea1d4dfb3483045a4e3f5ed5cae6a0d1
Ahmad-Elmahallawy <ahmad.mahallawy@gmail.com>
2024-04-12 01:28:12 -0400

    added time slots

--- frontend/src/Components/Reservations/Calendar.tsx
+++ frontend/src/Components/Reservations/Calendar.tsx
@@ -1,8 +1,8 @@
+import React, { useEffect, useState } from "react";
 import "../../Style/ReservationStyle/CalendarStyle.css";
-
 import FullCalendar from "@fullcalendar/react";
-import dayGridPlugin from "@fullcalendar/daygrid"; // a plugin
-import interactionPlugin from "@fullcalendar/interaction"; // needed for dayClick
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import ReservationModal from "./ReservationModal";
 import { useNavigate } from "react-router-dom";
@@ -23,17 +23,17 @@ export default function Calendar() {
   const [selectedDate, setSelectedDate] = useState("");
   const [events, setEvents] = useState<CalendarEvent[]>([]);
   const user = JSON.parse(localStorage.getItem("userData") || "{}");
-
   useEffect(() => {
     const fetchEvents = async () => {
       try {
         const response = await axios.get(
-          `${process.env.REACT_APP_API_URL}/reservations`, 
+          `${process.env.REACT_APP_API_URL}/reservations`, // URL
           {
             params: {
+              // Any query parameters go here, if needed
               where: {
                 user: { id: user.id },
-              },
+              },  
             },
             headers: { Authorization: `Bearer ${user.accessToken}` },
           }
@@ -53,11 +53,10 @@ export default function Calendar() {
     };
 
     fetchEvents();
-  }, []);
-
-
+  }, []); // Empty dependency array ensures this runs once on component mount
   const handleDateClick = (arg: any) => {
-    alert(arg.dateStr);
+    setSelectedDate(arg.dateStr);
+    setDialogOpen(true);
   };
 
   const handleEventCreation = async (
@@ -112,21 +111,16 @@ export default function Calendar() {
     );
   }
 
-  function handleEventClick(clickInfo: any) {
-    if (
-      window.confirm(
-        `Are you sure you want to delete the event '${clickInfo.event.title}'`
-      )
-    ) {
-      clickInfo.event.remove();
-    }
-  }
-
   return (
     <div className="calendar-container">
       <div className="calendar-heading">
         <h2>Common Facilities Calendar</h2>
-        <button className="my-reservations-btn">My Reservations</button>
+        <button
+          className="my-reservations-btn"
+          onClick={navigateToMyReservations}
+        >
+          My Reservations
+        </button>
       </div>
       <FullCalendar
         plugins={[dayGridPlugin, interactionPlugin, timeGridPlugin]}
@@ -136,22 +130,24 @@ export default function Calendar() {
           right: "dayGridMonth,timeGridWeek,timeGridDay",
         }}
         initialView="dayGridMonth"
-        events={[
-          { title: "event 1", date: "2024-04-01" },
-          { title: "event 2", date: "2024-04-02" },
-        ]}
-        dateClick={handleDateClick} // handle day clicks
+        events={events.map((event) => ({
+          title: event.title,
+          date: event.date,
+        }))}
+        dateClick={handleDateClick}
         eventContent={renderEventContent}
         themeSystem="standard"
         editable={true}
         selectable={true}
         selectMirror={true}
         dayMaxEvents={true}
-        eventClick={handleEventClick}
+      />
+      <ReservationModal
+        open={dialogOpen}
+        onClose={() => setDialogOpen(false)}
+        onSubmit={handleEventCreation}
+        defaultDate={selectedDate}
       />
     </div>
   );
-}
-function createEventId() {
-  throw new Error("Function not implemented.");
 }
--- frontend/src/Components/Reservations/ReservationModal.tsx
+++ frontend/src/Components/Reservations/ReservationModal.tsx
@@ -41,6 +41,47 @@ const ReservationModal: React.FC<EventCreationDialogProps> = ({
   onSubmit,
   defaultDate,
 }) => {
+  const customTextFieldStyle = {
+    "& .MuiOutlinedInput-root": {
+      color: "var(--color4)",
+      "& fieldset": {
+        borderColor: "var(--color3)",
+      },
+
+      "&.Mui-focused": {
+        backgroundColor: "transparent", // Background color when focused
+        color: "var(--color3)", // Text color when focused
+        "& .MuiOutlinedInput-notchedOutline": {
+          borderColor: "var(--color3)",
+          boxShadow: "none", // This removes the elevation effect
+        },
+        "& .MuiInputLabel-root": {
+          color: "#747264", // Label color when focused
+        },
+        "& .MuiInputLabel-shrink": {
+          backgroundColor: "transparent", // Ensuring the background of the shrunken label is transparent
+        },
+      },
+    },
+    "& .MuiInputLabel-root": {
+      color: "#3c3633", // Custom color for TextField Label
+      "&.Mui-focused": {
+        color: "#747264", // Color of the TextField label when focused
+      },
+    },
+    "&:hover .MuiInputLabel-root": {
+      color: "var(--color3)",
+    },
+    "& fieldset": { backgroundColor: "transparent" },
+  };
+
+  const customButtonStyle = {
+    backgroundColor: "var(--color3)",
+    color: "var(--color1)",
+    "&:hover": {
+      backgroundColor: "var(--color4)",
+    },
+  };
   const [name, setName] = useState("");
   const [unitNumber, setUnitNumber] = useState("");
   const [facility, setFacility] = useState("");
@@ -102,25 +143,61 @@ const ReservationModal: React.FC<EventCreationDialogProps> = ({
 
   return (
     <Dialog open={open} onClose={onClose}>
-      <DialogTitle>Common Facility Reservation</DialogTitle>
+      <DialogTitle sx={{ color: "var(--color4)" }}>
+        Common Facility Reservation
+      </DialogTitle>
       <DialogContent>
-        <Typography>User: {name}</Typography>
+        <Typography variant="h6" sx={{ color: "var(--color4)" }}>
+          User: {name}
+        </Typography>
         <TextField
           margin="dense"
           id="unitNumber"
           label="Unit Number"
           fullWidth
           value={unitNumber}
+          sx={customTextFieldStyle}
           onChange={(e) => setUnitNumber(e.target.value)}
         />
-        <FormControl fullWidth margin="dense">
-          <InputLabel id="facility-label">Facility</InputLabel>
+        <FormControl fullWidth margin="dense" sx={customTextFieldStyle}>
+          <InputLabel
+            id="facility-label"
+            sx={{
+              color: "#3c3633",
+              "&.Mui-focused": {
+                color: "#747264",
+              },
+            }}
+          >
+            Facility
+          </InputLabel>
           <Select
             labelId="facility-label"
             id="facility"
             value={facilityId}
             label="Facility"
             onChange={handleFacilityChange}
+            sx={{
+              "& .MuiSelect-select": {
+                color: "#3c3633",
+              },
+
+              "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                borderColor: "#747264",
+              },
+            }}
+            MenuProps={{
+              PaperProps: {
+                sx: {
+                  "& .MuiMenuItem-root:hover": {
+                    backgroundColor: "#747264",
+                  },
+                  "& .MuiMenuItem-root.Mui-selected": {
+                    backgroundColor: "#e0ccbe",
+                  },
+                },
+              },
+            }}
           >
             {facilities.map((facility) => (
               <MenuItem key={facility.id} value={facility.id}>
@@ -129,20 +206,71 @@ const ReservationModal: React.FC<EventCreationDialogProps> = ({
             ))}
           </Select>
         </FormControl>
-        <FormControl fullWidth margin="dense">
-          <InputLabel id="time-label">Time</InputLabel>
+        <FormControl fullWidth margin="dense" sx={customTextFieldStyle}>
+          <InputLabel
+            id="time-label"
+            sx={{
+              color: "#3c3633",
+              "&.Mui-focused": {
+                color: "#747264",
+              },
+            }}
+          >
+            Time
+          </InputLabel>
           <Select
             labelId="time-label"
             id="time"
             value={time}
             label="Time"
             onChange={(e) => setTime(e.target.value)}
-          ></Select>
+            sx={{
+              "& .MuiSelect-select": {
+                color: "#3c3633",
+              },
+
+              "&.Mui-focused .MuiOutlinedInput-notchedOutline": {
+                borderColor: "#747264",
+              },
+            }}
+            MenuProps={{
+              PaperProps: {
+                sx: {
+                  "& .MuiMenuItem-root:hover": {
+                    backgroundColor: "#747264",
+                  },
+                  "& .MuiMenuItem-root.Mui-selected": {
+                    backgroundColor: "#e0ccbe",
+                  },
+                },
+              },
+            }}
+          >
+            {Array.from({ length: 13 }, (_, i) => {
+              const startHour = 8 + i; // Start from 8 AM
+              const endHour = startHour + 1; // End hour for each slot
+              const startAmPm = startHour >= 12 ? "PM" : "AM";
+              const endAmPm = endHour >= 12 ? "PM" : "AM";
+              const formattedStartHour =
+                startHour > 12 ? startHour - 12 : startHour;
+              const formattedEndHour = endHour > 12 ? endHour - 12 : endHour;
+              const timeString = `${formattedStartHour}:00 ${startAmPm} - ${formattedEndHour}:00 ${endAmPm}`;
+              return (
+                <MenuItem key={timeString} value={timeString}>
+                  {timeString}
+                </MenuItem>
+              );
+            })}
+          </Select>
         </FormControl>
       </DialogContent>
       <DialogActions>
-        <Button onClick={onClose}>Cancel</Button>
-        <Button onClick={handleSubmit}>Reserve Facility</Button>
+        <Button onClick={onClose} sx={customButtonStyle}>
+          Cancel
+        </Button>
+        <Button onClick={handleSubmit} sx={customButtonStyle}>
+          Reserve Facility
+        </Button>
       </DialogActions>
     </Dialog>
   );
