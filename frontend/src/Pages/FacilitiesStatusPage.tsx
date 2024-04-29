032d9cf71067551776ceaad2764a18829654857f
Ahmad-Elmahallawy <ahmad.mahallawy@gmail.com>
2024-04-29 19:13:00 -0400

    fixed button to navigate to facility

--- frontend/src/Pages/FacilitiesStatusPage.tsx
+++ frontend/src/Pages/FacilitiesStatusPage.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import api from "../api";
 import IndividualPropertyFacilities from "../Components/CommonFacilities/IndividualFacility";
+import { Navigate, useNavigate } from "react-router-dom";
 
 export interface SinglePropertyFacilities {
   createdAt: string;
@@ -25,7 +26,7 @@ export interface PropertyByIdResponse {
 const FacilitiesStatusPage = () => {
   const user = JSON.parse(localStorage.getItem("userData") || "{}");
   const [properties, setProperties] = useState<PropertyByIdResponse[]>([]);
-
+  const nav = useNavigate();
   useEffect(() => {
     let mounted = true;
     api.properties.getProperties(user.accessToken).then((data) => {
@@ -43,6 +44,7 @@ const FacilitiesStatusPage = () => {
     <div className="common-facilities-landing-page">
       <div className="common-facilities-header">
         <h1>Common Facilities Status</h1>
+        <button onClick={()=> {nav("/CommonFacility")}}>Create A Facility</button>
       </div>
       <div className="common-facilities-content">
         {properties.map((property) => {
